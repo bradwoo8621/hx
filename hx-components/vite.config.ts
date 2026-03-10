d7ba703f00cd692d12ca95158a59ca796854071a
bradwoo8621 <bradwoo8621@users.noreply.github.com>
2026-03-10 21:33:39 +0800

    Initial commit

--- hx-components/vite.config.ts
+++ hx-components/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
 		dts({
 			tsconfigPath: './tsconfig.json',
 			entryRoot: 'src',
+			exclude: ['src/stories/**', 'src/**/*.stories.ts'],
 			outDir: 'dist/types'
 		})
 	],
