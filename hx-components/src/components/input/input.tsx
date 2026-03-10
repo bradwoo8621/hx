a86a536c4d099a035d7ba50a6a8198f41225dbd6
bradwoo8621 <bradwoo8621@users.noreply.github.com>
2026-03-10 20:43:24 +0800

    Initial commit

---
+++ hx-components/src/components/input/input.tsx
@@ -0,0 +1,50 @@
+import {
+	type ChangeEvent,
+	type ChangeEventHandler,
+	type DetailedHTMLProps,
+	type FocusEventHandler,
+	type ForwardedRef,
+	forwardRef,
+	type InputHTMLAttributes
+} from 'react';
+import {HxInputDefaults} from './defaults.ts';
+
+export interface HxExtInputProps {
+	selectAll?: boolean;
+	onChange?: (value: string | null | undefined, e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export type OmittedInputHTMLProps = 'onChange';
+
+export type HxInputProps =
+	HxExtInputProps
+	& Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, OmittedInputHTMLProps>
+
+export const HxInput = forwardRef((props: HxInputProps, ref: ForwardedRef<HTMLInputElement>) => {
+	const {selectAll = HxInputDefaults.selectAll, onFocus, onChange, ...rest} = props;
+
+	let onInputFocus: FocusEventHandler<HTMLInputElement> | undefined = (void 0);
+	if (selectAll || onFocus != null) {
+		onInputFocus = (ev) => {
+			if (selectAll) {
+				ev.target.select();
+			}
+			if (onFocus != null) {
+				onFocus(ev);
+			}
+		};
+	}
+
+	const onInputChange: ChangeEventHandler<HTMLInputElement> = (ev) => {
+		let value: string | undefined = ev.target.value;
+		if (value.length === 0) {
+			value = (void 0);
+		}
+		onChange?.(value, ev);
+	};
+
+	return <input {...rest}
+	              onFocus={onInputFocus} onChange={onInputChange}
+	              data-hx-input
+	              ref={ref}/>;
+});
