a069e310efb89a58f21813b4e122920ab1c80370
bradwoo8621 <bradwoo8621@users.noreply.github.com>
2026-03-12 01:37:02 +0800

    Initial commit

--- hx-components/src/contexts/theme-context.tsx
+++ hx-components/src/contexts/theme-context.tsx
@@ -58,15 +58,20 @@ class TC implements ThemeContext {
 		this.switchTo('light');
 	}
 
-	switchTo(themeCode: string): void {
+	private disableSystemThemeChangeMonitor(): void {
 		if (TC.SystemThemeChangeMonitored) {
 			TC.SystemThemeChangeMonitored = false;
 			TC.MediaQuery.removeEventListener('change', TC.SystemThemeChangeHandle);
 		}
+	}
+
+	switchTo(themeCode: string): void {
+		this.disableSystemThemeChangeMonitor();
 		TC.SwitchTo(themeCode);
 	}
 
 	clear(): void {
+		this.disableSystemThemeChangeMonitor();
 		TC.ClearTheme();
 	}
 }
@@ -74,7 +79,52 @@ class TC implements ThemeContext {
 const Context = createContext<ThemeContext>({} as ThemeContext);
 Context.displayName = 'HxThemeContext';
 
-export const ThemeProvider = (props: { children?: ReactNode }) => {
+/**
+ * Provide a context to its children, enabling them to call functions exposed by the theme context to switch or clear the theme.
+ * The predefined theme codes are `"light"` and `"dark"`. Additionally, custom themes can be defined via CSS by the user.
+ *
+ * In theory, all hx DOM nodes reside within either `div[data-hx-root]` or `div[data-hx-portal-root]`.
+ * If a theme code is designated:
+ * - `div[data-hx-root][data-hx-theme=dark]` indicates the dark theme is active.
+ * - `div[data-hx-root][data-hx-theme=light]` indicates the light theme is active.
+ * - `div[data-hx-root][data-hx-theme=custom]` indicates a custom theme is active.
+ * The same logic applies to the `div[data-hx-portal-root]` element for the `data-hx-theme` attribute.
+ * When the theme is cleared, the `data-hx-theme` attribute is removed from
+ * both `div[data-hx-root]` and `div[data-hx-portal-root]`.
+ *
+ * Based on the above implementation, switching the theme does not trigger a re-render of child nodes.
+ * It only controls CSS by modifying the attributes of the root nodes.
+ *
+ * Note that this will only affect `div[data-hx-root]` and `div[data-hx-portal-root]`, not the entire page.
+ *
+ * @example
+ * ```tsx
+ * <ThemeProvider>
+ *     ...
+ * </ThemeProvider>
+ *
+ * const SomeChild = () => {
+ *     // in child node
+ *     const theme = useTheme();
+ *
+ *     const switchTheme = () => {
+ *         // monitor the system theme change, auto switch to "dark" or "light"
+ *         theme.system();
+ *         // switch to "dark", and disable the system theme change monitor
+ *         theme.dark();
+ *         // switch to "light", and disable the system theme change monitor
+ *         theme.light();
+ *         // switch to specific theme, and disable the system theme change monitor
+ *         theme.switchTo("theme-code");
+ *         // clear theme, and disable the system theme change monitor
+ *         theme.clear();
+ *     }
+ *
+ *     return <button onClick={switchTheme}/>
+ * }
+ * ```
+ */
+export const ThemeProvider = (props: { children: ReactNode }) => {
 	const {children} = props;
 
 	const [context] = useState<ThemeContext>(() => new TC());
