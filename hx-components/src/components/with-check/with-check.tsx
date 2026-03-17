f00ae2020bec5abdf615390226a6172e6c7a4e8e
bradwoo8621 <bradwoo8621@users.noreply.github.com>
2026-03-17 11:14:47 +0800

    Initial commit

--- hx-components/src/components/with-check/with-check.tsx
+++ hx-components/src/components/with-check/with-check.tsx
@@ -1,18 +1,95 @@
 // @ts-ignore
-import React, {type FC, type ForwardedRef, forwardRef, type PropsWithoutRef} from 'react';
-import type {CheckProps} from '../../types';
+import React, {type FC, type ForwardedRef, forwardRef, type PropsWithoutRef, useEffect, useState} from 'react';
+import {type CheckPropSuppliedOn, useCheckMonitor} from '../../hooks';
+import type {CheckProps, ComponentDataProps} from '../../types';
+import {HxI18NLabel} from '../i18n-label';
 
-/** origin component for high-order with check */
-export type HxWithCheckOrgFC<P extends object> = FC<P>;
-/** props of high-order with check component */
-export type HxWithCheckPropsWithoutRef<T extends object, P extends object> = PropsWithoutRef<P & CheckProps<T>>;
+/**
+ * simplify given supplyOn:
+ * - remove all null and empty string elements if given supplyOn is an array
+ * - return undefined when given supplyOn is one of follows:
+ *   - null,
+ *   - empty string,
+ *   - empty array,
+ * - return array
+ */
+const simplifySupplyOn = (supplyOn?: CheckPropSuppliedOn): CheckPropSuppliedOn | undefined => {
+	if (supplyOn == null) {
+		return (void 0);
+	} else if (Array.isArray(supplyOn)) {
+		const filtered = supplyOn.filter(path => path != null && path.length !== 0);
+		return filtered.length === 0 ? (void 0) : (filtered.length === 1 ? filtered[0] : [...new Set(filtered)]);
+	} else {
+		return supplyOn.length === 0 ? (void 0) : (supplyOn.length === 1 ? supplyOn[0] : [...new Set(supplyOn)]);
+	}
+};
+
+/**
+ * compare the old and new values, return true if they are not same.
+ * make sure each element is unique if given old and new values are array.
+ *
+ * @param oldValue
+ * @param newValue
+ */
+const shouldUpdateSupplyOn = (oldValue?: CheckPropSuppliedOn, newValue?: CheckPropSuppliedOn): boolean => {
+	if (oldValue == null) {
+		// old: null. returns true when new is not null
+		return newValue != null;
+	} else if (newValue == null) {
+		// old: not null. always returns true when new is null
+		return true;
+	} else if (typeof oldValue === 'string') {
+		// old: string. returns true when new not equals old, new might be string or array
+		return oldValue !== newValue;
+	} else if (typeof newValue === 'string') {
+		// old: array. always returns true when new is string
+		return true;
+	} else if (oldValue.length !== newValue.length) {
+		// both are array, length not equals
+		return true;
+	} else {
+		const oldKeys = oldValue.reduce((acc, key) => {
+			acc[key] = 1;
+			return acc;
+		}, {} as Record<string, 1>);
+		for (let index = 0, len = newValue.length; index < len; index++) {
+			const v = oldKeys[newValue[index]];
+			if (v == null) {
+				// path exists in new, not exists in old
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+export interface HxWithCheckCreateOptions<T extends object, P extends ComponentDataProps<T>> {
+	$supplyOn?: (props: P) => CheckPropSuppliedOn;
+}
+
 export const HxWithCheck =
-	<P extends object>(C: HxWithCheckOrgFC<P>) => {
-		return forwardRef(<T extends object>(props: HxWithCheckPropsWithoutRef<T, P>, ref: ForwardedRef<HTMLDivElement>) => {
-			const {$check, ...rest} = props;
+	<T extends object, P extends ComponentDataProps<T>>(C: FC<P>, options?: HxWithCheckCreateOptions<T, P>) => {
+		return forwardRef(
+			(props: PropsWithoutRef<P & CheckProps<T>>, ref: ForwardedRef<HTMLDivElement>) => {
+				const {$model, $check, ...rest} = props;
 
-			return <div data-hx-check-box="" ref={ref}>
-				<C {...rest as any}/>
-			</div>;
-		});
+				const [supplyOn, setSupplyOn] = useState<CheckPropSuppliedOn | undefined>(() => {
+					// @ts-ignore
+					return simplifySupplyOn(options?.$supplyOn?.(props));
+				});
+				useEffect(() => {
+					// @ts-ignore
+					const newSupplyOn = simplifySupplyOn(options?.$supplyOn?.(props));
+					const shouldUpdate = shouldUpdateSupplyOn(supplyOn, newSupplyOn);
+					if (shouldUpdate) {
+						setSupplyOn(newSupplyOn);
+					}
+				}, [options?.$supplyOn, props]);
+				const {error} = useCheckMonitor({$model, $check, $supplyOn: supplyOn});
+
+				return <div data-hx-check-box="" ref={ref}>
+					<C {...rest as any} $model={$model}/>
+					<HxI18NLabel label={error?.message ?? ''}/>
+				</div>;
+			});
 	};
--- hx-components/src/hooks/use-data-monitor/index.ts
+++ hx-components/src/hooks/use-data-monitor/index.ts
@@ -1,4 +1,10 @@
-export {type UseDataMonitorOptions, type UseDataMonitorResult} from './types';
+export {
+	type UseDataMonitorOptions,
+	type UseDataMonitorResult,
+	type CheckPropSuppliedOn,
+	type UseCheckMonitorOptions,
+	type UseCheckMonitorResult
+} from './types';
 
 export * from './use-data-monitor';
 export * from './use-check-monitor';
