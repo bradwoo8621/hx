b4bdafa440648c03d73910069b238ae451986c0d
bradwoo8621 <bradwoo8621@users.noreply.github.com>
2026-07-16 13:54:23 +0800

    feat: export non-Gregorian move functions from datetime-picker barrel

--- hx-components/src/components/datetime-picker/index.ts
+++ hx-components/src/components/datetime-picker/index.ts
@@ -3,3 +3,9 @@ export {
 } from './defaults';
 export * from './types';
 export * from './datetime-picker';
+
+export {
+	moveYearToWhenNotGregorian, moveMonthToWhenNotGregorian, moveDayToWhenNotGregorian,
+	moveMonthForwardToInSameYearWhenNotGregorian, moveMonthBackwardToInSameYearWhenNotGregorian,
+	changeMonthToWhenNotGregorian, changeYearToWhenNotGregorian,
+} from './datetime-picker-popup-utils';
